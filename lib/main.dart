74cd4d158022a65ec7a1e7dbb8c6134f4003c8d9
mrudula <mrudulavaji11@gmail.com>
2024-03-31 10:04:22 +0530

    this is new project

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+      ),
+      home: ScreenHome(),
+    );
+  }
+}
+
+class ScreenHome extends StatelessWidget {
+   ScreenHome({super.key});
+
+
+    final _textController= TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+              // left: 20,
+              // right: 20,
+              // top: 20,
+              // bottom: 20,
+            
+            child: Container(
+              color: Colors.purple[100],
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Type Something....'
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      //get data
+                      print(_textController.text);
+                    },
+                    child: Text('Click here'),
+                  ),
+                  Text('Data Will Be Shown here'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
